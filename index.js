08f1c6629165c98c0a13165026a62d010fb26ab2
georgielill <georgielills@gmail.com>
2024-02-02 19:02:19 +0000

    added questions object with inputs and lists inside

--- index.js
+++ index.js
@@ -4,7 +4,50 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
+
+// - The title of my project
+// - Sections entitled: / 
+// - Description / 
+// - Table of Contents / 
+// - Installation /
+// - Usage /
+// - License /
+// - Contributing
+// - Tests
+// - Questions
 const questions = [
+  {
+    type: 'input',
+    message: 'What is the Title of your project?',
+    name: 'title',
+  },
+  {
+    type:'input',
+    message:'Enter a description of your project',
+    name: 'description',
+  },
+  {
+    type: 'input', 
+    message: 'Enter installation instructions',
+    name: 'installation'
+  },
+  {
+    type: 'input',
+    message: 'Enter usage instructions',
+    name: 'usage'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What license did you use?',
+    choices: ['MIT License', 'The Unlicense', 'Apache License 2.0'],
+  },
+  {
+    type: 'input',
+    name: 'contributions',
+    message: 'Enter contribution guidelines',
+    choices: ['email', 'text', 'phone'], 
+  }
 
 ];
 
